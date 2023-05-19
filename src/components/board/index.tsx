99f15c45ded33fbf1b72ca53626cd98fdba6b724
HenriqueDev06 <johanhenrique18@gmail.com>
2023-05-19 15:23:04 -0300

    Create BoardComponent and Styles

---
+++ src/components/board/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Card from '../card';
+import { BoardContainer } from './styes';
+
+interface Card {
+  id: number;
+  image: string;
+  flipped: boolean;
+}
+
+interface BoardProps {
+  cards: Card[];
+  handleCardClick: (id: number) => void;
+}
+
+const Board: React.FC<BoardProps> = ({ cards, handleCardClick }) => {
+  return (
+    <BoardContainer className="board">
+      {cards.map((card: Card) => (
+        <Card
+          key={card.id}
+          id={card.id}
+          image={card.image}
+          flipped={card.flipped}
+          onClick={handleCardClick}
+        />
+      ))}
+      a
+    </BoardContainer>
+  );
+};
+
+export default Board;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import { Container } from './styles';
 
 const Home: FC = () => (
   <Container>
-    <p>first page works! </p>
+    <p>first page works! a </p>
   </Container>
 );
 
