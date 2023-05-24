86758566345a69c7b22d083a044160a9cae3bc7c
HenriqueDev06 <johanhenrique18@gmail.com>
2023-05-23 23:55:20 -0300

    Edit Package Json

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
-
-reportWebVitals();
